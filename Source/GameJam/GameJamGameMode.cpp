a44c883c606766d864a8a6ac2e56cb43ff388a0e
Daniel Gillies <danny@skully.com>
2017-09-22 20:08:23 -0700

    First commit

---
+++ Source/GameJam/GameJamGameMode.cpp
@@ -0,0 +1,10 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "GameJamGameMode.h"
+#include "GameJamCharacter.h"
+
+AGameJamGameMode::AGameJamGameMode()
+{
+	// Set default pawn class to our character
+	DefaultPawnClass = AGameJamCharacter::StaticClass();	
+}
